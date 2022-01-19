ec51022931f4404149466aa4018dd767f7b4b8f1
frontdevruso <frontdevruso@gmail.com>
2022-01-19 09:34:27 +0200

    first prod commit

--- src/assets/js/inputmask.js
+++
@@ -1,9 +0,0 @@
-import IMask from 'imask';
-
-const tels = document.querySelectorAll("input[type=\"tel\"]");
-
-tels.forEach(el => {
-    IMask(el, {
-        mask: "+{7} (000) 000 0000"
-    });
-});
--- src/assets/js/search-mobile-bar.js
+++
@@ -1,9 +0,0 @@
-const searchBar = document.getElementById('searchBar');
-const searchBarBtn = document.getElementById('searchBarBtn');
-const body = document.body;
-
-searchBarBtn.addEventListener('click',function() {
-    if(!body.classList.contains('m-open')) {
-        searchBar.classList.toggle('header__searchbar--open');
-    }
-});
