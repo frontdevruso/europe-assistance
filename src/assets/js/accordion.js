cdd818baa897338aeb90d554388342c017f4bbad
frontdevruso <frontdevruso@gmail.com>
2022-01-22 12:54:39 +0300

    account-page

--- src/assets/js/accordion.js
+++ src/assets/js/accordion.js
@@ -26,36 +26,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-
-/* document.addEventListener("DOMContentLoaded", () => {
-    const accordions = document.querySelectorAll(".accordion");
-
-    accordions.forEach(el => {
-        el.addEventListener("click", (e) => {
-            const self = e.currentTarget;
-            const control = self.querySelector(".accordion__control");
-            const content = self.querySelector(".accordion__content");
-
-            self.classList.toggle("open");
-
-            const watchHeight = () => {
-                content.style.maxHeight = content.scrollHeight + "px";
-            };
-
-            window.addEventListener("resize", () => {
-                watchHeight();
-            });
-
-            // если открыт аккордеон
-            if (self.classList.contains("open")) {
-                control.setAttribute("aria-expanded", true);
-                content.setAttribute("aria-hidden", false);
-                watchHeight();
-            } else {
-                control.setAttribute("aria-expanded", false);
-                content.setAttribute("aria-hidden", true);
-                content.style.maxHeight = null;
-            }
-        });
-    });
-}); */
--- src/assets/js/main-slider.js
+++ src/assets/js/main-slider.js
@@ -1,19 +1,21 @@
-new Swiper(".main-slider", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
-    breakpoints: {
-      575: {
-        navigation: {
-          nextEl: ".main-slider-pagination-arrow--next",
-          prevEl: ".main-slider-pagination-arrow--prev",
-        },
+if (document.querySelector('.main-slider')) {
+  new Swiper(".main-slider", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+  
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-    }
-});
+  
+      breakpoints: {
+        575: {
+          navigation: {
+            nextEl: ".main-slider-pagination-arrow--next",
+            prevEl: ".main-slider-pagination-arrow--prev",
+          },
+        },
+      }
+  });
+}
