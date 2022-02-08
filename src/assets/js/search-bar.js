3b1a235870c247bc81eabc9af1b889e4afafb04d
frontdevruso <frontdevruso@gmail.com>
2022-02-08 23:29:23 +0300

    searchBar refactoring

--- src/assets/js/search-bar.js
+++ src/assets/js/search-bar.js
@@ -1,12 +1,32 @@
+const headerSearch = document.querySelector('.header__nav-search');
 const headerLinks = document.querySelector('.header__nav-links');
-const headerSearch = document.getElementById('headerSearch');
+const headerSearchBar = document.getElementById('headerSearch');
+const headerSearchBtn = document.getElementById('headerSearchBtn');
 
-headerSearch.addEventListener('focus', function() {
+headerSearchBar.addEventListener('focus', function(e) {
     headerLinks.classList.add('header__nav-links-hidden');
     document.getElementById("headerSearchIcon").style.transform = "rotate(90deg)";
+    e.target.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            // HERE SHOULD BE A REQUEST FOR SEARCHING TO THE SERVER !!!
+            if (headerSearchBar.value.length === 0) {
+                return
+            } else { alert('Производится поиск...') }
+        }
+    });
 });
 
-headerSearch.addEventListener('blur', function() {
-    headerLinks.classList.remove('header__nav-links-hidden');
-    document.getElementById("headerSearchIcon").style.transform = "rotate(0deg)";
+document.addEventListener('click', function(event) {
+    let isClickInsideElement = headerSearch.contains(event.target);
+    if (!isClickInsideElement) {
+        headerLinks.classList.remove('header__nav-links-hidden');
+        document.getElementById("headerSearchIcon").style.transform = "rotate(0deg)";
+    }
 });
+
+headerSearchBtn.addEventListener('click', function() {
+    // HERE SHOULD BE A REQUEST FOR SEARCHING TO THE SERVER !!!
+    if (headerSearchBar.value.length === 0) {
+        return
+    } else { alert('Производится поиск...') }
+})
